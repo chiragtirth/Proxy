c45b2e2916d7add8715a2a99cf2a334d385f29f5
chompypotato <fletcher.hoch@outlook.com>
2025-03-10 14:42:36 -0400

    Create tabcloak.js

---
+++ active/tabcloak.js
@@ -0,0 +1,15 @@
+ // Define tabCloak() globally so it can be called anywhere
+         function tabCloak() {
+            var newTitle = localStorage.getItem('cloakedTitle');
+            var newIcon = localStorage.getItem('cloakedIcon');
+
+            if (newTitle === null || newTitle === '') {
+                alert('No Cloak Detected. Please select one in settings.');
+            } else {
+                localStorage.setItem('tabTitle', newTitle);
+                localStorage.setItem('tabIcon', newIcon);
+                document.title = newTitle;
+                var icon = document.querySelector('link[rel="icon"]');
+                icon.setAttribute('href', newIcon);
+            }
+         }
