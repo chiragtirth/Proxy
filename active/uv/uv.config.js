ed0ceda5903c6c2958ed174ad6e4125cc03d7f56
chompypotato <fletcher.hoch@outlook.com>
2025-07-08 08:06:08 -0400

    Update uv.config.js

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/active/go/',
-    bare:'https://schoolwork-nine.vercel.app/',
+    bare:'https://bare2.mysticmath.workers.dev',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/active/uv/uv.handler.js',
